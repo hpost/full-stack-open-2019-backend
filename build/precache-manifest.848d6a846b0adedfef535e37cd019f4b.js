eba8338b7ded2169025df2b8327082f75b862a1e
Henning Post <henningpost@gmail.com>
2020-01-18 12:25:49 -0800

    uibuild

---
+++ build/precache-manifest.848d6a846b0adedfef535e37cd019f4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e5fd5513cb0839643c21b2e834737c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf814ee1bc437e20dfc3",
+    "url": "/static/js/2.0dc0cc14.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.0dc0cc14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06d257b6263580ea4c90",
+    "url": "/static/js/main.8a071e1c.chunk.js"
+  },
+  {
+    "revision": "f7fc8eba76c28cae35bb",
+    "url": "/static/js/runtime-main.a53a739b.js"
+  }
+]);
