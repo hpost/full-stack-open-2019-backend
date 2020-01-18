3e3a8c5d11f9fe975fd76afa6b8b451dad0cdc26
Henning Post <henningpost@gmail.com>
2020-01-17 21:18:09 -0800

    phonebook backend step4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const persons = [
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -36,6 +36,12 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
